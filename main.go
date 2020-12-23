7d1dddaea08df054887225166dfb4cccbdfb75e1
Saidou <57505334+souaremsaidou@users.noreply.github.com>
2020-12-23 17:01:18 +0100

    Update main.go

--- main.go
+++ main.go
@@ -62,27 +62,67 @@ func main() {
 
 	/*
 	 * Arrays
-	 * fixed size 
+	 * fixed size
 	 * similar type
 	 */
-	
-	// long syn
-	var arr[3] int
-	arr[0] = 1
+
+	//var arr[3] int
+	//arr[0] = 1
+	// or
+	arr := [3]int{1, 2, 3}
 	fmt.Println(arr) // fmt.Println(arr[1])
- 
-	
-	
-	
 
 	/*
 	 * Slices
 	 * dynamic size
 	 */
 
+	// slice from built-in array
+	slice := arr[:]
+	arr[0] = -1
+	slice[2] = 0
+	fmt.Println(arr, slice)
+
+	// implicit
+	slice2 := []int{1, 2, 3}
+	fmt.Println(slice2)
+
+	// slice is not fixed size
+	// std::vector in c++
+	slice2 = append(slice2, 4, 42)
+
+	// not include index 2
+	s := slice2[1:4]
+	fmt.Println(slice2, s)
+
+	/*
+	 * Maps
+	 * key, value
+	 */
+
+	m := map[string]int{"key": 0}
+	// var m map[string]int //panic: assignment to entry in nil map
+	m["key"] = 1 // map[key:1]
+	//delete(m, "key")
+	fmt.Println(m) // map[]
+
 	/*
 	 * Structs
 	 * fixed number of fields
 	 */
+	type MyStruct struct {
+		ID        int
+		FirstName string
+	}
+
+	var me MyStruct
+	//me.FirstName  = "Saidou"
+
+	you := MyStruct{
+		ID:        1,
+		FirstName: "Diamilatou",
+	}
+
+	fmt.Println(me, you)
 
 }
